817b02a5ecbd11c2d03656674cfd3c020e7b8251
Jerry <Jerry@Boru.Li>
2019-05-03 15:28:53 -0400

    first commit

---
+++ app/src/main/java/ese543/helpmi/core/NewPostActivity.java
@@ -0,0 +1,15 @@
+package ese543.helpmi.core;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ese543.helpmi.R;
+
+public class NewPostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_post);
+    }
+}
---
+++ app/src/main/java/ese543/helpmi/startup/LoginActivity.java
@@ -0,0 +1,26 @@
+package ese543.helpmi.startup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import ese543.helpmi.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void onclick_login(View view)
+    {
+
+    }
+
+    public void onclick_createUser(View view)
+    {
+
+    }
+}
