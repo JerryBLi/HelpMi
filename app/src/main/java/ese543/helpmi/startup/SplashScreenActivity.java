b9f1f4407220d3b4c28e14e08d3be1f4987e9206
Jerry <Jerry@Boru.Li>
2019-05-05 13:28:12 -0400

    added splash screen

--- app/src/main/java/ese543/helpmi/startup/SplashScreenActivity.java
+++ app/src/main/java/ese543/helpmi/startup/SplashScreenActivity.java
@@ -1,15 +1,34 @@
 package ese543.helpmi.startup;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
+import ese543.helpmi.MainActivity;
 import ese543.helpmi.R;
 
 public class SplashScreenActivity extends AppCompatActivity {
-
+    private static int SPLASH_TIME_OUT = 1500;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 }
